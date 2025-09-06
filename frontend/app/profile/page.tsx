218c8583621203015736fe06450887dc62d1c8b9
itsmesaadali <itmesaad@gmail.com>
2025-09-06 12:21:23 +0500

    user retrived into profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,10 +1,33 @@
+import axios from "axios";
+import { cookies } from "next/headers";
 
-const profielPage = () => {
-  return (
-    <div>
-        Profile
-    </div>
-  )
+type Response = {
+  message:string,
+  user:{
+    id:number;
+    name:string;
+    email:string;
+  }
 }
 
-export default profielPage
+const getUser = async () => {
+  const cookieStore = await cookies();
+  const accessToken = (await cookieStore.get("access_token"))?.value;
+  const refreshToken = (await cookieStore.get("refresh_token"))?.value;
+  const res = await axios.get("/user/me", {
+    withCredentials: true,
+    headers: {
+      Authorization: `access_token=${accessToken}, refresh_token=${refreshToken}`,
+    },
+  });
+
+  const data = await res.data as Response;
+  return data;
+};
+
+const profielPage = async () => {
+  const user = await getUser()
+  return <div>{JSON.stringify(user)}</div>;
+};
+
+export default profielPage;
