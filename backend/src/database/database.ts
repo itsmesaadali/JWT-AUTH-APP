a293df7ce5e0b2a560cfa9ed82cb540a09cce02f
itsmesaadali <itmesaad@gmail.com>
2025-09-24 20:44:10 +0500

    Remove from serveless enviroment

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,23 +1,18 @@
-import mongoose from "mongoose";
-import { config } from "../config/app.config";
-
-let isConnected = false; // track the connection state
+import mongoose from 'mongoose'
+import { config } from '../config/app.config'
 
 const connectDB = async () => {
-  if (isConnected) {
-    return;
-  }
+    try {
+        await mongoose.connect(config.MONGO_URI, {
+        });
+        console.log('Connect to DB')
+    } catch (error) {
+        console.log(error)
+        console.log('Failed to Connect DB')
+        process.exit(1)
+        
+    }
+}
 
-  try {
-    await mongoose.connect(config.MONGO_URI, {
-    });
-    isConnected = true;
-    console.log("✅ Connected to MongoDB");
-  } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:", error);
-    // ❌ Don't use process.exit(1) on Vercel
-    throw error;
-  }
-};
 
 export default connectDB;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,22 @@
 import app from "./app";
 import connectDB from "./database/database";
+import { config } from "./config/app.config";
 
-let isConnected = false;
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server isNot running =>", error);
+      throw error;
+    });
 
-const initDB = async () => {
-  if (!isConnected) {
-    try {
-      await connectDB();
-      isConnected = true;
-      console.log("MongoDB connected successfully ✅");
-    } catch (error) {
-      console.error("MongoDB connection failed ❌", error);
-    }
-  }
-};
-
-initDB();
+      app.listen(config.PORT, async () => {
+      console.log(
+        `Server listening on port ${config.PORT} in ${config.NODE_ENV}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDb connection failed", error);
+  });
 
 export default app;
