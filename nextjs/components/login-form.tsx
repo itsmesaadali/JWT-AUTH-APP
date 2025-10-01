ae45914e7365928ec43264fae5e5149a2dfb4198
itsmesaadali <itmesaad@gmail.com>
2025-10-01 12:26:19 +0500

    fix the bugs

--- nextjs/components/login-form.tsx
+++ nextjs/components/login-form.tsx
@@ -21,6 +21,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import Loading from "./loading";
 
 import { signIn } from "../server/users";
 
@@ -28,21 +29,22 @@ import { z } from "zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Loader2 } from "lucide-react";
 import { authClient } from "../lib/auth-client";
 import Link from "next/link";
 
 const formSchema = z.object({
   email: z.string().email("Invalid email"),
-  password: z.string().min(8, {message: 'Password must contain at least 8 character(s)'}),
+  password: z
+    .string()
+    .min(8, { message: "Password must contain at least 8 character(s)" }),
 });
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-
   const [isloading, setIsloading] = useState(false);
+  const [isGoogleLoading, setIsGoogleLoading] = useState(false);
 
   const router = useRouter();
 
@@ -54,27 +56,35 @@ export function LoginForm({
     },
   });
 
- const signInWithGoogle = async () => {
-  await authClient.signIn.social({
-    provider:'google',
-    callbackURL:'/dashboard',
-  })
- } 
+  const signInWithGoogle = async () => {
+    try {
+      setIsGoogleLoading(true);
+      await authClient.signIn.social({
+        provider: "google",
+        callbackURL: "/dashboard",
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Google login failed");
+    } finally {
+      setIsGoogleLoading(false);
+    }
+  };
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      setIsloading(true);
+      const { success, message } = await signIn(values.email, values.password);
 
-    setIsloading(true);
-    const { success, message} = await signIn(values.email, values.password);
-
-    if(success) {
-      toast.success(message as string);
-      router.push('/dashboard')
-    } else {
-      toast.error(message as string);
+      if (success) {
+        toast.success(message as string);
+        router.push("/dashboard");
+      } else {
+        toast.error(message as string);
+      }
+    } finally {
+      setIsloading(false);
     }
-
-    setIsloading(false);
-    
   }
 
   return (
@@ -82,23 +92,39 @@ export function LoginForm({
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>
-            Login with your Google account
-          </CardDescription>
+          <CardDescription>Login with your Google account</CardDescription>
         </CardHeader>
         <CardContent>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
-                  <Button variant="outline" className="w-full" type="button" onClick={signInWithGoogle}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    Login with Google
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    type="button"
+                    onClick={signInWithGoogle}
+                    disabled={isGoogleLoading}
+                  >
+                    {isGoogleLoading ? (
+                      <span className="flex items-center justify-center gap-2">
+                        <Loading />Login...
+                      </span>
+                    ) : (
+                      <>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="h-4 w-4"
+                        >
+                          <path
+                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        Login with Google
+                      </>
+                    )}
                   </Button>
                 </div>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -131,15 +157,8 @@ export function LoginForm({
                       name="password"
                       render={({ field }) => (
                         <FormItem>
-                          <div className="flex items-center">
-                            <FormLabel>Password</FormLabel>
-                            <Link
-                              href="#"
-                              className="ml-auto text-sm underline-offset-4 hover:underline"
-                            >
-                              Forgot your password?
-                            </Link>
-                          </div>
+                          <FormLabel>Password</FormLabel>
+
                           <FormControl>
                             <Input
                               type="password"
@@ -147,6 +166,12 @@ export function LoginForm({
                               {...field}
                             />
                           </FormControl>
+                          <Link
+                            href="#"
+                            className="ml-auto text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </Link>
                           <FormMessage />
                         </FormItem>
                       )}
@@ -155,7 +180,7 @@ export function LoginForm({
 
                   {/* Submit Button */}
                   <Button type="submit" className="w-full" disabled={isloading}>
-                    { isloading ? <Loader2 className="animate-spin size-4"/> : 'Login'}
+                    {isloading ? <Loading /> : "Login"}
                   </Button>
                 </div>
 
@@ -171,8 +196,9 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[Link]:hover:text-primary text-center text-xs text-balance *:[Link]:underline *:[Link]:underline-offset-4">
-        By clicking continue, you agree to our <Link href="#">Terms of Service</Link>{" "}
-        and <Link href="#">Privacy Policy</Link>.
+        By clicking continue, you agree to our{" "}
+        <Link href="#">Terms of Service</Link> and{" "}
+        <Link href="#">Privacy Policy</Link>.
       </div>
     </div>
   );
--- nextjs/components/signup-form.tsx
+++ nextjs/components/signup-form.tsx
@@ -21,6 +21,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import Loading from "./loading";
 
 import { signUp } from "../server/users";
 
@@ -28,12 +29,13 @@ import { z } from "zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Loader2 } from "lucide-react";
 import { authClient } from "../lib/auth-client";
 import Link from "next/link";
 
 const formSchema = z.object({
-  username: z.string().min(3, { message: "Username must contain at least 3 character(s)" }),
+  username: z
+    .string()
+    .min(3, { message: "Username must contain at least 3 character(s)" }),
   email: z.string().email("Invalid email"),
   password: z
     .string()
@@ -45,6 +47,7 @@ export function SignUpForm({
   ...props
 }: React.ComponentProps<"div">) {
   const [isloading, setIsloading] = useState(false);
+  const [isGoogleLoading, setIsGoogleLoading] = useState(false);
 
   const router = useRouter();
 
@@ -58,24 +61,38 @@ export function SignUpForm({
   });
 
   const signInWithGoogle = async () => {
-    await authClient.signIn.social({
-      provider: "google",
-      callbackURL: "/dashboard",
-    });
+    try {
+      setIsGoogleLoading(true);
+      await authClient.signIn.social({
+        provider: "google",
+        callbackURL: "/dashboard",
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Google login failed");
+    } finally {
+      setIsGoogleLoading(false);
+    }
   };
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    setIsloading(true);
-    const { success, message } = await signUp(values.username , values.email, values.password);
+    try {
+      setIsloading(true);
+      const { success, message } = await signUp(
+        values.username,
+        values.email,
+        values.password
+      );
 
-    if (success) {
-      toast.success(message as string);
-      router.push("/dashboard");
-    } else {
-      toast.error(message as string);
+      if (success) {
+        toast.success(message as string);
+        router.push("/dashboard");
+      } else {
+        toast.error(message as string);
+      }
+    } finally {
+      setIsloading(false);
     }
-
-    setIsloading(false);
   }
 
   return (
@@ -83,9 +100,7 @@ export function SignUpForm({
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>
-            Signup with your Google account
-          </CardDescription>
+          <CardDescription>Signup with your Google account</CardDescription>
         </CardHeader>
         <CardContent>
           <Form {...form}>
@@ -97,14 +112,27 @@ export function SignUpForm({
                     className="w-full"
                     type="button"
                     onClick={signInWithGoogle}
+                    disabled={isGoogleLoading}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    Signup with Google
+                    {isGoogleLoading ? (
+                      <span className="flex items-center justify-center gap-2">
+                        <Loading />Signup...
+                      </span>
+                    ) : (
+                      <>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="h-4 w-4"
+                        >
+                          <path
+                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        Signup with Google
+                      </>
+                    )}
                   </Button>
                 </div>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -153,15 +181,8 @@ export function SignUpForm({
                       name="password"
                       render={({ field }) => (
                         <FormItem>
-                          <div className="flex items-center">
-                            <FormLabel>Password</FormLabel>
-                            <Link
-                              href="#"
-                              className="ml-auto text-sm underline-offset-4 hover:underline"
-                            >
-                              Forgot your password?
-                            </Link>
-                          </div>
+                          <FormLabel>Password</FormLabel>
+
                           <FormControl>
                             <Input
                               type="password"
@@ -169,6 +190,12 @@ export function SignUpForm({
                               {...field}
                             />
                           </FormControl>
+                          <Link
+                            href="#"
+                            className="ml-auto text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </Link>
                           <FormMessage />
                         </FormItem>
                       )}
@@ -177,11 +204,7 @@ export function SignUpForm({
 
                   {/* Submit Button */}
                   <Button type="submit" className="w-full" disabled={isloading}>
-                    {isloading ? (
-                      <Loader2 className="animate-spin size-4" />
-                    ) : (
-                      "Signup"
-                    )}
+                    {isloading ? <Loading /> : "Signup"}
                   </Button>
                 </div>
 
@@ -197,8 +220,9 @@ export function SignUpForm({
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[Link]:hover:text-primary text-center text-xs text-balance *:[Link]:underline *:[Link]:underline-offset-4">
-        By clicking continue, you agree to our <Link href="#">Terms of Service</Link>{" "}
-        and <Link href="#">Privacy Policy</Link>.
+        By clicking continue, you agree to our{" "}
+        <Link href="#">Terms of Service</Link> and{" "}
+        <Link href="#">Privacy Policy</Link>.
       </div>
     </div>
   );
